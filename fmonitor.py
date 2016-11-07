29bff761582b96d2bbbc6da8ed0ea8d070096890
YanhaoMo <moyanhao@ooutlook.com>
2016-11-07 23:01:19 +0800

    update

---
+++ fmonitor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Fmonitor:
+    """
+    File monitor
+    """
+
+    def __init_(self):
+        self.debList = []
+        self.dscList = []
+
+    def monitor(self):
+        pass
