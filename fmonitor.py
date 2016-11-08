54b7a683cd48d80dca0cd94e6e1e1c18e5d40646
YanhaoMo <moyanhao@ooutlook.com>
2016-11-08 15:18:39 +0800

    update

--- fmonitor.py
+++ fmonitor.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from watchdog.observers import Observer
+from warchdog.events import FileSystemEventHandler
+
+
+class ModifyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        pass
+
 
 class Fmonitor:
     """
     File monitor
     """
 
-    def __init_(self):
+    def __init_(self, directory):
+        self.__monitorDir = directory
         self.debList = []
         self.dscList = []
 
     def monitor(self):
-        pass
+        observer = Observer()
+        event_handler = ModifyHandler()
+        observer.schedule(event_handler, self.__monitorDir)
+        observer.start()
